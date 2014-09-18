09201d8152c610efc80e36fa1cd572ddf7ecf277
Ming-Ying Chung <itsmings@gmail.com>
2014-09-18 15:34:54 -0400

    Update system zsh

--- system/locale.zsh
+++ system/locale.zsh
@@ -1,2 +1,2 @@
-export LANG=zh_TW.UTF-8
-export LC_ALL=zh_TW.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
--- system/source.zsh
+++ system/source.zsh
@@ -1,5 +1,11 @@
-# Load `pythonbrew`
-[[ -s $HOME/.pythonbrew/etc/bashrc ]] && source $HOME/.pythonbrew/etc/bashrc 
-
 # Load `nvm`
 [[ -s $HOME/.nvm/nvm.sh ]] && source $HOME/.nvm/nvm.sh
+
+# Load `pyenv`
+[[ -s $HOME/.pyenv ]] && export PYENV_ROOT=$HOME/.pyenv
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+
+# Add RVM to PATH for scripting
+[[ -s $HOME/.rvm ]] && export PATH=$PATH:$HOME/.rvm/bin
+# Load RVM into a shell session *as a function*
+[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm 
