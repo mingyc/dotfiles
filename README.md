4eab47181b237e0191a24e118ab35bb0bf945383
Ming-Ying Chung <itsmings@gmail.com>
2022-07-28 12:27:25 +0000

    Migrate to chezmoi - the modern dotfiles manager
    This repo is previously managed by @holman's custom zsh framework. This
    change migrates to use [chezmoi] instead.
    
    Also removes unused configs and commands. The new setup is roughly:
    
    - zsh: ohmyzsh framework. Custom configs are stored as chezmoi
      templates.
    - vim: Vundle plugin manager. vimrc is downloaded from another repo.
    - gdbinit: downloaded from another repo.
    
    [chezmoi]: https://chezmoi.io

---
+++ README.md
@@ -0,0 +1,12 @@
+# dotfiles
+---
+
+My dotfiles managed with [chezmoi](https://chezmoi.io).
+
+
+## Installation
+
+```sh
+sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply mingyc
+```
+
---
+++ home/.chezmoitemplates/zsh/env.zsh
@@ -0,0 +1,7 @@
+export PATH="$PATH:$HOME/.local/bin"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export EDITOR='vim'
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
