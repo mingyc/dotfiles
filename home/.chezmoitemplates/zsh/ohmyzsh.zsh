a68e94cb82c2c51c69c9737f54f2f150b9f30cde
Ming-Ying Chung <itsmings@gmail.com>
2022-08-06 20:20:07 +0900

    mv `dot_dotfiles/*` `dot_config/`

--- home/.chezmoitemplates/zsh/ohmyzsh.zsh
+++ home/.chezmoitemplates/zsh/ohmyzsh.zsh
@@ -1,14 +1,14 @@
 # Set up https://ohmyz.sh
-ZSH=$HOME/.dotfiles/oh-my-zsh
+ZSH=$HOME/.config/oh-my-zsh
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 DISABLE_AUTO_UPDATE="true"
-ZSH_CUSTOM=$HOME/.dotfiles/zsh/custom
+ZSH_CUSTOM=$HOME/.config/zsh/custom
 ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
 HIST_STAMPS="yyyy-mm-dd"
 
-# Plugins from $ZSH/plugins/* or $ZSH/custom/plugins/*
+# Plugins from $ZSH/plugins/* or $ZSH_CUSTOM/plugins/*
 plugins=(
   history
   zsh-vi-mode
