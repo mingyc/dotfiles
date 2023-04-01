d09f47c126a6022fa5ce3fc9bc1d251099492657
Ming-Ying Chung <itsmings@gmail.com>
2023-04-01 20:19:58 +0900

    Untemplate `env.zsh`

--- home/dot_config/zsh/custom/env.zsh
+++ home/dot_config/zsh/custom/env.zsh
@@ -1,6 +1,8 @@
 export PATH="$PATH:$HOME/.local/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
+typeset -gU PATH
+
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
